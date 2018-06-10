a435750f44899979e9a1bc68c2f8f69bb9c57deb
staticfloat@gmail.com <staticfloat@gmail.com>
2018-06-10 16:49:15 -0700

    Add tests for downloading `.tar.bz2` and `.tar.xz` files

--- test/runtests.jl
+++ test/runtests.jl
@@ -615,17 +615,23 @@ end
 end
 
 const socrates_urls = [
-    "https://github.com/staticfloat/small_bin/raw/master/socrates.tar.gz" => "e65d2f13f2085f2c279830e863292312a72930fee5ba3c792b14c33ce5c5cc58",
-    "https://github.com/staticfloat/small_bin/raw/master/socrates.tar.bz2" => "13fc17b97be41763b02cbb80e9d048302cec3bd3d446c2ed6e8210bddcd3ac76",
-    "https://github.com/staticfloat/small_bin/raw/master/socrates.tar.xz" => "61bcf109fcb749ee7b6a570a6057602c08c836b6f81091eab7aa5f5870ec6475",
+    "https://github.com/staticfloat/small_bin/raw/f1a92f5eafbd30a0c6a8efb6947485b0f6d1bec3/socrates.tar.gz" =>
+    "e65d2f13f2085f2c279830e863292312a72930fee5ba3c792b14c33ce5c5cc58",
+    "https://github.com/staticfloat/small_bin/raw/f1a92f5eafbd30a0c6a8efb6947485b0f6d1bec3/socrates.tar.bz2" =>
+    "13fc17b97be41763b02cbb80e9d048302cec3bd3d446c2ed6e8210bddcd3ac76",
+    "https://github.com/staticfloat/small_bin/raw/f1a92f5eafbd30a0c6a8efb6947485b0f6d1bec3/socrates.tar.xz" =>
+    "61bcf109fcb749ee7b6a570a6057602c08c836b6f81091eab7aa5f5870ec6475",
 ]
 const socrates_hash = "adcbcf15674eafe8905093183d9ab997cbfba9056fc7dde8bfa5a22dfcfb4967"
 
 @testset "Downloading" begin
-    for url, hash in socrates_urls
+    for (url, hash) in socrates_urls
         temp_prefix() do prefix
-            @test download_verify_unpack(url, hash, prefix; verbose=true)
+            Base.rm(prefix.path; recursive=true, force=true)
+            download_verify_unpack(url, hash, prefix.path; verbose=true)
             socrates_path = joinpath(prefix, "bin", "socrates")
+            @test isfile(socrates_path)
+
             unpacked_hash = open(socrates_path) do f
                 bytes2hex(sha256(f))
             end
