2f540fe83779d03445b87b16df0abcd9feed0202
Ian Butterworth <i.r.butterworth@gmail.com>
2019-07-03 01:08:57 -0400

    Show failed dlopen result in verbose mode (#164)
    * show failed dlopen result in verbose mode
    
    * try catch to get error

--- src/Products.jl
+++ src/Products.jl
@@ -178,7 +178,11 @@ function locate(lp::LibraryProduct; verbose::Bool = false,
                     end
 
                     if verbose
-                        @info("$(dl_path) cannot be dlopen'ed")
+                        try
+                            dlopen(dl_path)
+                        catch dlopen_result
+                            @info("$(dl_path) cannot be dlopen'ed",dlopen_result)
+                        end
                     end
                 else
                     # If the current platform doesn't match, then just trust in our
